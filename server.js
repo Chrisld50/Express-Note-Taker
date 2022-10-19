d639c1f36293ccf1a66a41a5b5d7c9b756c4b69a
Chrisld50 <chrisld50@yahoo.com>
2022-10-18 20:59:27 -0500

    got the notetaker to work, the server running and the notes to write to the html page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
+const fs = require('fs');
 const express = require('express');
-const path = require ('path');
+const path = require('path');
 
 const app = express();
 
@@ -11,19 +11,36 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 app.post('/api/notes', (req, res) => {
-
+    let noteEl = req.body
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err
+        let database = JSON.parse(data)
+        database.push(noteEl)
+        console.log(database)
+        fs.writeFile('./db/db.json', JSON.stringify(database), err => {
+            if(err) throw err;
+            console.log('database updated')
+        })
+    }) 
+   res.redirect('/notes')
 })
 
+app.delete('/api/notes', (req, res) =>{
+    req.unlink('./db/db.json', noteEl, err =>{
+        if(err)throw err
+        console.log('Note deleted!')
+    })
+})
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, '/public/index.html'))
+}); 
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-app.get('api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'))
 });
 
